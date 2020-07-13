2b531c0cb6cd40b859f00be4870bbbb2253530e3
Antonio Hadrović <hadrovic1@gmail.com>
2020-07-13 19:21:58 +0200

    Add fetch

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import apiURL from './apiURL'
+import apiURL from './apiURL';
+import Results from './Results'
 
 export default class Search extends Component {
     constructor(props) {
@@ -12,7 +13,7 @@ export default class Search extends Component {
             //future value from repos
         }
         this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
@@ -20,16 +21,28 @@ export default class Search extends Component {
         console.log(this.state)
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        fetch(apiURL + this.state.value)
+            .then(res => res.json())
+        
+            .then(value => this.setState({valueFromUsers: value}))
+
+            console.log(this.state)
+    }
+
+    
     render() {
         return (
             <div>
                 <form>
-                    <label onSubmit={this.handleSumbit}>
+                    <label onSubmit={this.handleSubmit}>
                         <input type='text' value={this.state.value} onChange={this.handleChange} />
                     </label>
                     <input type='submit' value='Submit search'></input>
                 </form>
-                
+                <Results />
             </div>
         )
     }
